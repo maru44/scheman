42bd7f60c5ae76fa2434a4fb3f11d60ab95058b2
maru44 <takumaru22244@gmail.com>
2022-03-01 07:44:25 +0900

    csv write maybe OK

--- definition/definition.go
+++ definition/definition.go
@@ -21,3 +21,8 @@ type (
 		RawMermaid         string
 	}
 )
+
+func (c *CommonInfo) IsShownAttr(attr string) bool {
+	_, ok := c.IgnoreAttributes[attr]
+	return ok
+}
--- file/csv.go
+++ file/csv.go
@@ -3,14 +3,28 @@ package file
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/friendsofgo/errors"
 )
 
-func (f *File) writeCSV(ctx context.Context) error {
+func (f *File) writeCSV(showAttrs []string, ctx context.Context) error {
 	file, err := os.OpenFile(f.definitionFile, os.O_APPEND|os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
 	if err != nil {
 		return errors.Wrap(err, "failed to open definition file")
+	}
+
+	var out string
+	rowsSlice := f.makeRowsSlice(showAttrs)
+	for _, tables := range rowsSlice {
+		for _, columns := range tables {
+			out += strings.Join(columns, ", ") + "\n"
+		}
+		out += "\n"
+	}
+
+	if _, err := file.Write([]byte(out)); err != nil {
+		return errors.Wrap(err, "failed to write definition file")
 	}
 
 	return nil
