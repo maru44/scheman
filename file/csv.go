1d6482a17a8419494e46325b1836055894b8abe9
maru44 <takumaru22244@gmail.com>
2022-02-28 19:58:43 +0900

    WIP csv

--- file/csv.go
+++ file/csv.go
@@ -1,8 +1,17 @@
 package file
 
-import "context"
+import (
+	"context"
+	"os"
+
+	"github.com/friendsofgo/errors"
+)
 
 func (f *File) writeCSV(ctx context.Context) error {
+	file, err := os.OpenFile(f.definitionFile, os.O_APPEND|os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+	if err != nil {
+		return errors.Wrap(err, "failed to open definition file")
+	}
 
 	return nil
 }
