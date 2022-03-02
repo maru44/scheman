c267268642bc6f12204cefb650a324ed26ba7186
maru44 <takumaru22244@gmail.com>
2022-03-03 07:21:34 +0900

    test ok

--- core/core_test.go
+++ core/core_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewSchemanState(t *testing.T) {
+func Test_SetDefinitionToSchemanState(t *testing.T) {
 	tests := []struct {
 		name            string
 		driverName      string
@@ -36,6 +36,57 @@ func Test_NewSchemanState(t *testing.T) {
 			wantIsFileDef:   false,
 			wantIsFileERD:   false,
 		},
+		{
+			name: "notion only erd",
+			envs: map[string]interface{}{
+				"services":       []string{},
+				"erd-outputs":    []string{string(ServiceNotion)},
+				"notion-page-id": "a",
+				"notion-token":   "a",
+				"def-file":       "a",
+				"erd-file":       "a",
+			},
+			wantIsNotionNil: false,
+			wantIsFileNil:   true,
+			wantIsNotionDef: false,
+			wantIsNotionERD: true,
+			wantIsFileDef:   false,
+			wantIsFileERD:   false,
+		},
+		{
+			name: "file only def",
+			envs: map[string]interface{}{
+				"services":       []string{string(ServiceFile)},
+				"erd-outputs":    []string{},
+				"notion-page-id": "a",
+				"notion-token":   "a",
+				"def-file":       "a",
+				"erd-file":       "a",
+			},
+			wantIsNotionNil: true,
+			wantIsFileNil:   false,
+			wantIsNotionDef: false,
+			wantIsNotionERD: false,
+			wantIsFileDef:   true,
+			wantIsFileERD:   false,
+		},
+		{
+			name: "all",
+			envs: map[string]interface{}{
+				"services":       []string{string(ServiceFile), string(ServiceNotion)},
+				"erd-outputs":    []string{string(ServiceFile), string(ServiceNotion)},
+				"notion-page-id": "a",
+				"notion-token":   "a",
+				"def-file":       "a",
+				"erd-file":       "a",
+			},
+			wantIsNotionNil: false,
+			wantIsFileNil:   false,
+			wantIsNotionDef: true,
+			wantIsNotionERD: true,
+			wantIsFileDef:   true,
+			wantIsFileERD:   true,
+		},
 	}
 
 	for _, tt := range tests {
@@ -44,10 +95,10 @@ func Test_NewSchemanState(t *testing.T) {
 			for k, v := range tt.envs {
 				viper.Set(k, v)
 			}
-
 			schemanState := &SchemanState{
 				Defs: map[Service]definition.Definition{},
 			}
+
 			ss, err := schemanState.setDefinition(&definition.CommonInfo{})
 			assert.NoError(t, err)
 			for _, s := range Services {
